f109eca438c265ac5a2b8046d5e18f347a42ee10
abdulloh-uzb <usmonovabdulloh87@gmail.com>
2024-10-15 23:12:54 +0500

    created form request for storing course

---
+++ app/Http/Requests/StoreCourseRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+use function PHPSTORM_META\map;
+
+class StoreCourseRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "name" => "required|max:255",
+            "branch_id" => "required|exists:branches,id",
+            "moduls" => "required|json",
+            "price" => "required|numeric|min:0"
+        ];
+    }
+}
