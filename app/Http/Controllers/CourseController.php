710698ba0519bb48fd6085e3c5a48eccffe183f1
abdulloh-uzb <usmonovabdulloh87@gmail.com>
2024-10-15 23:19:09 +0500

    finished index, create and store method for courses

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreCourseRequest;
+use App\Models\Course;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    //
+    public function index()
+    {
+        $courses = Course::all();
+        return view("courses.index", $courses);
+    }
+
+    public function create()
+    {
+        return view("course.create");
+    }
+
+    public function store(StoreCourseRequest $request)
+    {
+        $validated = $request->validate();
+        Course::create($validated);
+        return redirect()->route("courses.index")->with("success", "Course successfully created!");
+    }
+
+    
 }
